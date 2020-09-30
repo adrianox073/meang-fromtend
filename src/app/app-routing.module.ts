91f679888f1a54d371d97d93cab9016633b49daf
Adriano <adrianoxy@gmail.com>
2020-09-30 21:33:03 +0100

    Curso MongoDB nodeJS cap 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './@public/pages/contact/contact.component';
+import { HomeComponent } from './@public/pages/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: `home`, loadChildren: () =>
+  //     import('./@public/pages/home/home.module').then(m => m.HomeModule)
+  // },
+  
+  // {
+  //   path: `contact`, loadChildren: () =>
+  //     import('./@public/pages/contact/contact.module').then(m => m.ContactModule)
+  // },
+  // { path: ``, redirectTo: `home`, pathMatch: `full` }
+
+
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true, scrollPositionRestoration: "enabled"})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
